fcb4944a5e1ee3f0d9d3ad6587dff7511d551f76
Kyle Kincer <kyle_kincer@sweetwater.com>
2024-12-06 22:18:12 -0500

    day 05 part 1

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"advent2024/solutions/day02"
 	"advent2024/solutions/day03"
 	"advent2024/solutions/day04"
+	"advent2024/solutions/day05"
 	"fmt"
 )
 
@@ -24,4 +25,8 @@ func main() {
 	// Day 4
 	part1, part2 = day04.Solve()
 	fmt.Printf("Day 4:\n  Part 1: %v\n  Part 2: %v\n", part1, part2)
+
+	// Day 5
+	part1, part2 = day05.Solve()
+	fmt.Printf("Day 5:\n  Part 1: %v\n  Part 2: %v\n", part1, part2)
 }
---
+++ solutions/day05/solution.go
@@ -0,0 +1,108 @@
+package day05
+
+import (
+	"advent2024/utils"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func Solve() (interface{}, interface{}) {
+	input := utils.ReadInputFile("solutions/day05/input.txt")
+	return solvePart1(input), solvePart2(input)
+}
+
+type Rule struct {
+	first  int
+	second int
+}
+
+type Update struct {
+	pages []int
+}
+
+func NewRule(s string) *Rule {
+	pagesStr := strings.Split(s, "|")
+	first, err := strconv.Atoi(pagesStr[0])
+	if err != nil {
+		panic(err)
+	}
+
+	second, err := strconv.Atoi(pagesStr[1])
+	if err != nil {
+		panic(err)
+	}
+	return &Rule{
+		first:  first,
+		second: second,
+	}
+}
+
+func NewUpdate(s string) *Update {
+	var pages []int
+	pagesStr := strings.Split(s, ",")
+	for _, pageStr := range pagesStr {
+		page, err := strconv.Atoi(pageStr)
+		if err != nil {
+			panic(err)
+		}
+		pages = append(pages, page)
+	}
+	return &Update{
+		pages: pages,
+	}
+}
+
+func (u *Update) ruleApplies(r *Rule) bool {
+	return slices.Contains(u.pages, r.first) && slices.Contains(u.pages, r.second)
+}
+
+func (u *Update) rulePasses(r *Rule) bool {
+	return slices.Index(u.pages, r.first) < slices.Index(u.pages, r.second)
+}
+
+func (u *Update) getMiddlePage() int {
+	return u.pages[int(len(u.pages)/2)]
+}
+
+func solvePart1(input []string) interface{} {
+	var rules []*Rule
+	var updates []*Update
+	for _, l := range input {
+		if strings.Contains(l, "|") {
+			rules = append(rules, NewRule(l))
+		}
+
+		if strings.Contains(l, ",") {
+			updates = append(updates, NewUpdate(l))
+		}
+	}
+
+	var passes []*Update
+	for _, update := range updates {
+		pass := true
+		for _, rule := range rules {
+			if !update.ruleApplies(rule) {
+				continue
+			}
+			if !update.rulePasses(rule) {
+				pass = false
+				break
+			}
+
+		}
+		if pass {
+			passes = append(passes, update)
+		}
+	}
+
+	var sum int
+	for _, update := range passes {
+		sum += update.getMiddlePage()
+	}
+	return sum
+}
+
+func solvePart2(input []string) interface{} {
+	return nil
+}
